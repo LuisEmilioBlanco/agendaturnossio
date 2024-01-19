468a4720cdbdfbc57f688cb6baa0ee3c17b79afc
Luis <luisemilioblanco@outlook.com>
2024-01-19 14:30:15 -0300

    Proyecto final JS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,64 @@
 let turnos = [];
 
-window.onload = function () {
-    cargarTurnosDesdeLocalStorage();
+$(document).ready(function () {
+    cargarDatosDesdeAPI();
     mostrarTurnos();
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const inputNombre = document.getElementById('nombrePacienteInput');
-        const botonAgregar = document.getElementById('agregarBtn');
+    const inputNombre = $('#nombrePacienteInput');
+    const botonAgregar = $('#agregarBtn');
+    const botonBuscarPorNombre = $('#buscarPorNombreBtn');
 
-        botonAgregar.addEventListener('click', function() {
-            agregarTurno(inputNombre.value);
-        });
+    botonAgregar.on('click', function () {
+        agregarTurno(inputNombre.val())
+            .then(() => {
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Turno Agregado!',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                mostrarTurnos();
+            })
+            .catch(error => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: error
+                });
+            });
+    });
 
-        function agregarTurno(nombrePaciente) {
+    botonBuscarPorNombre.on('click', function () {
+        const nombreABuscar = inputNombre.val();
+        buscarTurnosPorNombre(nombreABuscar)
+            .then(resultadosNombre => mostrarResultados(resultadosNombre))
+            .catch(error => {
+                console.error('Error al buscar por nombre:', error);
+            });
+    });
+
+    $(document).on('click', '.eliminarBtn', function () {
+        const index = $(this).data('index');
+        eliminarTurno(index);
+    });
+
+    function cargarDatosDesdeAPI() {
+        const urlAPI = 'https://randomuser.me/api/';
+
+        fetch(urlAPI)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Datos de la API:', data);
+                const paciente = `${data.results[0].name.first} ${data.results[0].name.last}`;
+                agregarTurno(paciente);
+            })
+            .catch(error => {
+                console.error('Error al cargar datos desde la API:', error);
+            });
+    }
+    
+    function agregarTurno(nombrePaciente) {
+        return new Promise((resolve, reject) => {
             if (nombrePaciente.trim() !== '') {
                 const nuevoTurno = {
                     paciente: nombrePaciente,
@@ -21,80 +67,120 @@ window.onload = function () {
                     estado: 'Pendiente'
                 };
                 turnos.push(nuevoTurno);
-                guardarTurnosEnLocalStorage();
-                inputNombre.value = '';
-                console.log('Nuevo turno agregado:', nuevoTurno);
-                mostrarTurnos();
+                guardarTurnosEnJSONStorage()
+                    .then(() => {
+                        inputNombre.val('');
+                        console.log('Nuevo turno agregado:', nuevoTurno);
+                        mostrarTurnos();
+                        resolve();
+                    })
+                    .catch(error => reject('Error al guardar en el almacenamiento: ' + error));
             } else {
-                alert('Por favor ingresa un nombre de paciente válido.');
+                reject('Por favor ingresa un nombre de paciente válido.');
             }
-        }
-
-    });
-};
-
-function guardarTurnosEnLocalStorage() {
-    localStorage.setItem('turnos', JSON.stringify(turnos));
-}
-
-function cargarTurnosDesdeLocalStorage() {
-    const turnosGuardados = localStorage.getItem('turnos');
-    if (turnosGuardados) {
-        turnos = JSON.parse(turnosGuardados);
-    }
-}
-
-function mostrarTurnos() {
-    const lista = document.getElementById('turnoList');
-    lista.innerHTML = ''; // Limpiamos la lista antes de agregar los nuevos turnos
-
-    turnos.forEach((turno, index) => {
-        const li = document.createElement('li');
-        li.textContent = `${index + 1}. Paciente: ${turno.paciente} - Hora: ${turno.hora} - Fecha: ${turno.fecha} - Estado: ${turno.estado}`;
-        lista.appendChild(li); // Agregamos el nuevo turno a la lista
-    });
-}
-
-function obtenerHoraActual() {
-    const fecha = new Date();
-    const hora = fecha.getHours();
-    const minutos = fecha.getMinutes();
-    return `${hora}:${minutos < 10 ? '0' + minutos : minutos}`;
-}
-
-function obtenerFechaActual() {
-    const fecha = new Date();
-    const dia = fecha.getDate();
-    const mes = fecha.getMonth() + 1;
-    const año = fecha.getFullYear();
-    return `${dia}/${mes}/${año}`;
-}
-
-function mostrarResultados(resultados) {
-    const listaResultados = document.getElementById('resultadosList');
-    listaResultados.innerHTML = '';
-    
-    if (resultados.length === 0) {
-        const li = document.createElement('li');
-        li.textContent = 'No se encontraron resultados';
-        listaResultados.appendChild(li);
-    } else {
-        resultados.forEach((turno, index) => {
-            const li = document.createElement('li');
-            li.textContent = `${index + 1}. Paciente: ${turno.paciente} - Fecha y hora: ${turno.fecha} ${turno.hora} - Estado: ${turno.estado}`;
-            listaResultados.appendChild(li);
         });
     }
-}
 
-function buscarTurnoPorNombre(nombre) {
-    return turnos.filter(turno => turno.paciente.toLowerCase().includes(nombre.toLowerCase()));
-}
+    function eliminarTurno(index) {
+        turnos.splice(index, 1);
+        guardarTurnosEnJSONStorage()
+            .then(() => {
+                mostrarTurnos();
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Turno Eliminado!',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.error('Error al guardar en el almacenamiento: ', error);
+            });
+    }
 
-function filtrarPorEstado(estado) {
-    if (estado === '') {
-        return turnos;
-    } else {
+    function guardarTurnosEnJSONStorage() {
+        return new Promise(resolve => {
+            localStorage.setItem('turnos', JSON.stringify(turnos));
+            resolve();
+        });
+    }
+
+    function cargarTurnosDesdeJSONStorage() {
+        return new Promise(resolve => {
+            const turnosGuardados = localStorage.getItem('turnos');
+            if (turnosGuardados) {
+                turnos = JSON.parse(turnosGuardados);
+            }
+            resolve();
+        });
+    }
+
+    function mostrarTurnos() {
+        const lista = $('#turnoList');
+        lista.empty();
+
+        const template = $('#turnoTemplate').html();
+        const compiledTemplate = Handlebars.compile(template);
+
+        turnos.forEach((turno, index) => {
+            const html = compiledTemplate({
+                index: index + 1,
+                paciente: turno.paciente,
+                hora: turno.hora,
+                fecha: turno.fecha,
+                estado: turno.estado
+            });
+            lista.append(html);
+        });
+    }
+
+    function obtenerHoraActual() {
+        const fecha = new Date();
+        const hora = fecha.getHours();
+        const minutos = fecha.getMinutes();
+        const minutosFormateados = (minutos < 10) ? '0' + minutos : minutos;
+        return `${hora}:${minutosFormateados}`;
+    }
+    
+
+    function obtenerFechaActual() {
+        const fecha = new Date();
+        const dia = fecha.getDate();
+        const mes = fecha.getMonth() + 1;
+        const año = fecha.getFullYear();
+        return `${dia}/${mes}/${año}`;
+    }
+
+    function mostrarResultados(resultados) {
+        const listaResultados = $('#resultadosList');
+        listaResultados.empty();
+
+        if (resultados.length === 0) {
+            const li = $('<li></li>').text('No se encontraron resultados');
+            listaResultados.append(li);
+        } else {
+            resultados.forEach((turno, index) => {
+                const li = $('<li></li>').text(`${index + 1}. Paciente: ${turno.paciente} - Fecha y hora: ${turno.fecha} ${turno.hora} - Estado: ${turno.estado}`);
+                listaResultados.append(li);
+            });
+        }
+    }
+
+    function buscarTurnosPorNombre(nombre) {
+        return turnos.filter(turno => turno.paciente.toLowerCase().includes(nombre.toLowerCase()));
+    }
+
+    function filtrarPorEstado() {
+        const estado = $('#estadoFiltro').val();
+        if (estado === '') {
+            mostrarTurnos();
+        } else {
+            const turnosFiltrados = filtrarTurnosPorEstado(estado);
+            mostrarTurnos(turnosFiltrados);
+        }
+    }
+
+    function filtrarTurnosPorEstado(estado) {
         return turnos.filter(turno => turno.estado === estado);
     }
-}
+});
